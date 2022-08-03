5afd9e5a355a78eb06f42d22c2c238e8158af028
hayden <haydenburger43@gmail.com>
2022-08-02 19:38:41 -0500

    added logout and login functionality: '

---
+++ public/logout.js
@@ -0,0 +1,20 @@
+<nav>
+<button id="logout" class="btn-no-style">logout</button>
+<a href="/login">login</a>
+</nav>
+
+
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#logout').addEventListener('click', logout);
